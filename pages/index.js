5164a32b8b44982f64385f7759bfc14cdb116df6
julioaires123 <carpinavendas41@gmail.com>
2022-10-26 20:25:18 -0300

    projeto

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import Head from 'next/head'
+
 export default function Home() {
+
+
   return (
-<div>
- 
-<h1>Página Principal</h1>
-</div>
+<>
+<Head>
+  <title>Página Principal</title>
+  <meta name="Keywords" content="palavra chave01, palavra chave02, palavra chave03, palavra chave04, "></meta>
+  <meta name="description" content="Descrição da sua empresa aqui!, "></meta>
+</Head>
+<div><h1>Olá Sejá Bem Vindo!!</h1></div>
+</>
   )
 }
