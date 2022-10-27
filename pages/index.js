d0f79096763ab9cd0ef15c4d72c7b3c0130458a9
julioaires123 <carpinavendas41@gmail.com>
2022-10-26 21:31:55 -0300

    adicionando imagens

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
@@ -10,7 +12,17 @@ export default function Home() {
   <meta name="Keywords" content="palavra chave01, palavra chave02, palavra chave03, palavra chave04, "></meta>
   <meta name="description" content="Descrição da sua empresa aqui!, "></meta>
 </Head>
-<div><h1>Olá Sejá Bem Vindo!!</h1></div>
+<div>
+  <h1 className = {styles.title}>Olá Sejá Bem Vindo!!</h1>
+  <p>Esse site está em construção!!</p>
+  <Image 
+  src="/images/02.jpg"
+  width="700px"
+  height="500px"
+  alt="Chave da Empresa"
+  />
+  
+  </div>
 </>
   )
 }
